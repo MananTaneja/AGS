ea9ce7a1e780e8e6899869f9086eaff52a4fab49
MananTaneja <manantaneja007@gmail.com>
2020-06-12 22:38:25 +0530

    added a route

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const app = express();
 
 const port = 5000;
 
-app.get('/try', (req, res) => {
-    res.json("Hi!");
+app.get('/generate/mcdonalds', (req, res) => {
+    res.redirect('http://localhost:3000/');
 })
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
