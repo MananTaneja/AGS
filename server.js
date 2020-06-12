d54d4405f64c25a1f3947617ffa35675210f4840
MananTaneja <manantaneja007@gmail.com>
2020-06-12 23:12:03 +0530

    added home page

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 
 const app = express();
-
+const path = require('path');
 const port = 5000;
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname+'/home.html'));
+})
 
 app.get('/generate/mcdonalds', (req, res) => {
     res.redirect('http://localhost:3000/');
