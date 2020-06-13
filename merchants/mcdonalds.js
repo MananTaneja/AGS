e1c9c147f5d46070dae0704fe02f06be2727a346
MananTaneja <manantaneja007@gmail.com>
2020-06-13 13:02:24 +0530

    added a few scripts

--- merchants/mcdonalds.js
+++ merchants/mcdonalds.js
@@ -2,7 +2,8 @@ const express = require('express')
 const router = express.Router();
 
 router.get('/:table_id', (req, res) => {
-    res.send('table number at mcd is: ' + req.params.table_id);
+    console.log('table number at mcd is: ' + req.params.table_id);
+    res.redirect("http://localhost:3000");
 });
 
 module.exports = router;
